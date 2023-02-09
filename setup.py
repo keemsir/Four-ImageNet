31e3b0118f924265d3f4cf8a800722d5d880f34a
keemsir <74044742+keemsir@users.noreply.github.com>
2023-02-09 15:17:48 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+VERSION = "0.0.1"
+
+setup(
+  name='Medical_Image_Processing',
+  version=VERSION,
+  url='https://github.com/keemsir/Medical_Image_Processing',
+  author='keemsir',
+  author_email='keemsir@gmail.com',
+  packages=['MIP'],
+  description='A library for processing the Medical image',
+  license='keemsir'
+)
